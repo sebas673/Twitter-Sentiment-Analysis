cfa66d9274830630797f20933b3c1938c04cffd7
Sebastian Guzman <sg35@princeton.edu>
2019-11-19 11:44:39 -0500

    timeline grows

--- twitter_analysis/models.py
+++ twitter_analysis/models.py
@@ -9,7 +9,6 @@ from django.contrib.postgres.fields import ArrayField
 
 class Campaign(models.Model):
 
-    # we'll need to add more fields eventually
     name = models.CharField(max_length=100, verbose_name="Name")
     description = models.TextField(verbose_name="Notes")
     last_refreshed = models.DateTimeField(auto_now=True)
@@ -52,67 +51,47 @@ class Campaign(models.Model):
         self.negative_percent0 = results0[1]
         self.neutral_percent0 = results0[2]
 
-        print("the new keyword is", self.keyword0)
-
         if len(results0[3]) != 0:
             self.posID0 = results0[3][0]
 
         if len(results0[4]) != 0:
             self.negID0 = results0[4][0]
 
+        # if the keyword has changed
         if self.old_keyword != self.keyword0:
             listPos = []
-            print("pos not match")
-
         else:
             # add new PositiveIDs to the ArrayField
             listPos = self.posIDs
 
-        # print(type(listPos))
         if listPos is None:
-            print("&&&&&&&&&&")
             self.posIDs = results0[3]
 
         else:
-            # print("listPost len before: ", len(listPos))
-            # print("len additions: ", len(results0[3]))
             listPos += results0[3]
-            # print("listPost len after: ", len(listPos))
             print("pos: ", listPos)
             self.posIDs = listPos
 
         if self.old_keyword != self.keyword0:
             listNeg = []
             self.old_keyword = self.keyword0
-            print("neg not match and clear")
-
         else:
-            # add new NegativeIDs to the ArrayField
             listNeg = self.negIDs
 
-        # print(type(listNeg))
         if listNeg is None:
-            print("##########")
-            # print(results0[4])
             self.negIDs = results0[4]
 
         else:
-            # print("listNeg len before: ", len(listNeg))
-            # print("len additions: ", len(results0[4]))
             listNeg += results0[4]
-            # print("listNeg len after: ", len(listNeg))
-            print("negL ", listNeg)
             self.negIDs = listNeg
 
-        # print("keyword: ", self.keyword0)
-        # print("positive: ", self.positive_percent0)
-        # print("negative: ", self.negative_percent0)
-        # print("neutral: ", self.neutral_percent0)
-        # print("total: ", self.positive_percent0 +
-        #       self.negative_percent0 + self.neutral_percent0)
-
         results_time = sentiment_by_time(self.keyword0, numTweets)
-        self.times = results_time[0]
-        self.polarities = results_time[1]
+        newTimes = self.times
+        newTimes += results_time[0]
+        self.times = newTimes
+
+        newPolarities = self.polarities
+        newPolarities += results_time[1]
+        self.polarities = newPolarities
 
         super().save(*args, **kwargs)
